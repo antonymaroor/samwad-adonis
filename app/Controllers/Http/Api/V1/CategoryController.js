1b4c93353ccccf9c4f0b57726048d6fec24094ea
Antony <antony@cedextech.com>
2020-01-13 13:02:53 +0530

    reponse changes

--- app/Controllers/Http/Api/V1/CategoryController.js
+++ app/Controllers/Http/Api/V1/CategoryController.js
@@ -5,31 +5,35 @@ class CategoryController {
 
   async index ({ request, response }) {
     const categories = await Category.query().whereNull('category_id').where('status',1).with('childCategories').fetch()
+    var resp = {'status':'success', 'data':categories}
    // response.send(categories.toJSON())
-   return response.status(200).json(categories)
+   return response.status(200).json(resp)
   }
 
   async show ({ request, response }) {
   	const category = await Category.find(request.params.category)
-
   	let data = '';
 
 
     if(category == null)
     {
-      data = {'error' : 'Category not found'}
+      var resp = {'status':'error','data':'Category not found'}
 
     }
   	else if(category.redirect_type == 'video') {
   		data = await category.videos().where('status',1).fetch()
+      var resp = {'status':'success', 'data':data}
+
   	}
   	else if(category.redirect_type == 'chat')
   	{
   		data = {chat_id : category.chat_id}
+      var resp = {'status':'success', 'data':data}
   	}
     else
     {
       data = {'error' : 'Please select a subcategory'}
+      var resp = {'status':'error', 'data':data}
     }  
 
   	return response.status(200).json(data)
